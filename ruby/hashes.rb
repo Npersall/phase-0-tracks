5ee9eb78f8df4d248af62afa1d98eb20cb567b41
Nathan Persall <npersall@gmail.com>
2017-02-24 14:11:42 -0800

    This was an error recommiting on propper branch.

---
+++ ruby/hashes.rb
@@ -0,0 +1,14 @@
+#collect info = name, age, number of children, decor theme, floor material(hardwood, carpet)
+
+
+puts "Please enter your name"
+ client_name = gets.chomp
+puts "Thank you #{client_name}Please enter your age."
+ client_age = gets.chomp
+puts "How many children do you have?"
+ number_of_children = gets.to_i
+puts "#{client_name} please select your decor theme"
+ decor_theme = gets.chomp
+puts "Do you want carpet or hardwood floors"
+ floor_choice = gets.chomp
+
