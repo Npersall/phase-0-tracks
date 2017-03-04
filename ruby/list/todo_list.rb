5bab9ddb6f6fa0e2a25cfe4be1104dda37f5efa1
Nathan Persall <npersall@gmail.com>
2017-03-03 17:25:49 -0800

    added/ updated calculator.rb calculator_spec.rb todo_list_spec.rb santa.rb-- made a mistake and was working on master.Forgot to change branch

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,20 @@
+class TodoList
+def initialize (list_array)
+  @todo_list = list_array
+end
+  def get_items
+    @todo_list
+  end
+  def add_item(added_item)
+    @todo_list << added_item
+  end
+  def delete_item(to_delete)
+    @todo_list.delete(to_delete)
+end
+  def get_item(index)
+    @todo_list[index]
+end
+end
+
+# list = TodoList.new(["do the dishes","mow the lawn "])
+# list.get_items
