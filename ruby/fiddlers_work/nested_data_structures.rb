edccbcc23126a7c4bfc04f2ae21c479b840e36ac
Nathan Persall <npersall@gmail.com>
2017-03-20 05:04:27 -0700

    initial commit of nested_data_structures.rb

---
+++ ruby/fiddlers_work/nested_data_structures.rb
@@ -0,0 +1,61 @@
+school = {
+ Classroom_1: {
+   pretty_name: 'Classroom 1',
+   student_count: {
+     filled_seats: 25,
+     absent: 2
+   },
+   room_contents: [
+     "pens",
+     "paper",
+     "bag lunch"
+     ]
+ },
+ classroom_2: {
+   pretty_name: 'Classroom 2',
+   student_count: {
+     filled_seats: 25,
+     absent: 1
+   },
+   room_contents: [
+     "textbook",
+     "notebook",
+     "pencils",
+     "bag lunch"
+     ]
+ },
+ classroom_4: {
+   pretty_name: 'Classroom 4',
+   student_count: {
+     filled_seats: 23,
+     absent: 2
+   },
+   room_contents: [
+     "textbook",
+     "notebook",
+     "pens",
+     "highlighters",
+     "bag lunch"
+     ]
+ },
+ classroom_5: {
+   pretty_name: 'classroom 5',
+   student_count: {
+     filled_seats: 0,
+     absent: 0
+   },
+   room_contents: []
+ }
+}
+
+#print the notebook in the Classroom 4
+
+school[:classroom_4][:room_contents][1]
+
+#add 1 student with a textbook and notebook for classroom 5
+
+school[:classroom_5][:student_count][:filled_seats] = 1
+
+school[:classroom_5][:room_contents].push('history_notebook', 'history_textbook')
+
+school[:classroom_5]
