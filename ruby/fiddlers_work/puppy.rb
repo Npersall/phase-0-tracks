cc093174a00b088491d88f3aed04a3ba5e3c48bf
Nathan Persall <npersall@gmail.com>
2017-03-22 15:49:03 -0700

    Add puppy.rb

---
+++ ruby/fiddlers_work/puppy.rb
@@ -0,0 +1,19 @@
+# SPECIES ------------------------
+# Chihuahua Terrier mix
+
+# CHARACTERISTICS ----------------
+# tail length: varies
+# Friendly: yes
+# paw count: 4
+# Name: varies
+# Color: varies
+# mix breed: varies
+#eye color: varies
+
+
+# BEHAVIOR -----------------------
+# Jump
+# fetch
+# roll
+# shake
+#sneeze 
