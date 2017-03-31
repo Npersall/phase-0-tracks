2e678136111a8841172e1458fc34894441931303
Nathan Persall <npersall@gmail.com>
2017-03-31 00:28:34 -0700

    added gps6

--- ruby/fiddlers_work/game.rb
+++ ruby/fiddlers_work/game.rb
@@ -11,8 +11,8 @@
 
 
 class Word_game
-    attr_reader :game_board, :answer, :word_index
-    attr_accessor :guessing_letter, :game_arr, :word_arr
+    attr_reader :game_board, :answer, :word_index, :word_arr
+    attr_accessor :guessing_letter, :game_arr
 
   def initialize(answer)
     @answer = answer
@@ -20,8 +20,8 @@ class Word_game
     @guess_count = 0
     @game_board = ""
     @word_index = 0
-    @game_arr = game_arr
-    @word_arr = word_arr
+    @game_arr = []
+    @word_arr = []
   end
   def word_entry
     @game_board = @hidden_word * @answer.length
@@ -33,11 +33,11 @@ class Word_game
     game_arr = @game_board.split(" ")
   end
   def guessing_letter(letter)
-    if word_arr.include?(letter)
-        word_arr.each_index do |x|
-          if letter == word_arr[x]
+    if @word_arr.include?(letter)
+        @word_arr.each_index do |x|
+          if letter == @word_arr[x]
             current_index = x
-            word_arr[current_index] = letter
+            @word_arr[current_index] = letter
             @game_board = game_arr.join(" ")
              end
           end
@@ -49,16 +49,14 @@ class Word_game
 
   end
 end
- 
 
 game = Word_game.new("hangman")
   game.word_entry
-   game.split_answer
-   game.split_table
+   p game.split_answer
+   p game.split_table
 
    puts "pick a letter"
     letter = gets.chomp.downcase
-
 
    game.guessing_letter(letter)
   p game.game_board
